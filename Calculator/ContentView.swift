2cd52e04af72d67e119b4833913eb275ef195284
Dexter Ng <DNg@lcs.on.ca>
2024-01-15 12:27:37 -0500

    finished the calculator for addition.

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Stored properties
+    @State var firstNumber: Int = 1
+    @State var secondNumber: Int = 1
+    
+    // MARK: Computed properties
+    var sum: Int {
+        return firstNumber + secondNumber
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            
+            Spacer()
+            
+            VStack(alignment: .trailing) {
+                
+                Spacer()
+                
+                    Text("\(firstNumber)")
+                        .font(.system(size: 50))
+                
+                Stepper(value: $firstNumber, label: {
+                    Text("select first number")
+                })
+
+                HStack {
+                    Text("+")
+                        .font(.system(size: 45))
+                    
+                    Spacer()
+                    
+                    Text("\(secondNumber)")
+                        .font(.system(size: 50))
+                }
+                
+                Stepper(value: $secondNumber, label: {
+                    Text("select second number")
+                })
+
+                Divider()
+                
+                    Text("\(sum)")
+                        .font(.system(size: 50))
+                            
+                Spacer()
+
+            }
+            .padding()
+            
         }
-        .padding()
     }
 }
 
