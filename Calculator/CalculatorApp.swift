9e5382200a37b3d088dce823cd9c7ead27ac6b09
Dexter Ng <DNg@lcs.on.ca>
2024-01-15 12:04:43 -0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Dexter Ng on 2024-01-15.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
